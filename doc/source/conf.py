b3aa540c5860250205ebd48aa5216c7845780b12
sr-dash <dash.soumya922@gmail.com>
2019-10-31 12:59:15 +0530

    AIA example added

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -75,7 +75,8 @@ html_theme_options = {
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
-    'titles_only': False
+    'titles_only': False,
+    'test-align': justify
 }
 
 
